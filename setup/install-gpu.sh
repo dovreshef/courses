8d1b76fbd414f1bc23495f6b4300f10c2e95f3f3
Dov Reshef <dov@appcard.com>
2017-06-18 15:43:26 +0300

    some fixes for python

--- setup/install-gpu.sh
+++ setup/install-gpu.sh
@@ -15,19 +15,28 @@ sudo apt-get -y install cuda
 sudo modprobe nvidia
 nvidia-smi
 
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3.6 python3.6-pip tmux
+
 # install Anaconda for current user
 mkdir downloads
 cd downloads
-wget "https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh" -O "Anaconda2-4.2.0-Linux-x86_64.sh"
-bash "Anaconda2-4.2.0-Linux-x86_64.sh" -b
 
-echo "export PATH=\"$HOME/anaconda2/bin:\$PATH\"" >> ~/.bashrc
-export PATH="$HOME/anaconda2/bin:$PATH"
-conda install -y bcolz
-conda upgrade -y --all
+python3 -m venv ~/.ml
+source ~/.ml/bin/activate
 
-# install and configure theano
-pip install theano
+pip3 install bcolz==1.1.2
+pip3 install h5py==2.7.0
+pip3 install jupyter==1.0.0
+pip3 install Keras==1.2.2
+pip3 install matplotlib==2.0.2
+pip3 install pandas==0.20.2
+pip3 install Pillow==4.1.1
+pip3 install pipdeptree==0.10.1
+pip3 install scikit-learn==0.18.1
+pip3 install tensorflow-gpu==1.1.0
+
 echo "[global]
 device = gpu
 floatX = float32
@@ -35,8 +44,6 @@ floatX = float32
 [cuda]
 root = /usr/local/cuda" > ~/.theanorc
 
-# install and configure keras
-pip install keras==1.2.2
 mkdir ~/.keras
 echo '{
     "image_dim_ordering": "th",
@@ -53,11 +60,21 @@ sudo cp lib64/* /usr/local/cuda/lib64/
 sudo cp include/* /usr/local/cuda/include/
 
 # configure jupyter and prompt for password
+
+export CERT_PATH=~/.ssh/cert.pem
+export KEY_PATH=~/.ssh/key.pem
+openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout "$KEY_PATH" -out "$CERT_PATH"
+
 jupyter notebook --generate-config
-jupass=`python -c "from notebook.auth import passwd; print(passwd())"`
-echo "c.NotebookApp.password = u'"$jupass"'" >> $HOME/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.ip = '*'
-c.NotebookApp.open_browser = False" >> $HOME/.jupyter/jupyter_notebook_config.py
+jupass=$(python -c "from notebook.auth import passwd; print(passwd())")
+export CONFIG=$HOME/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.password = u'"$jupass"'" >> $CONFIG
+echo "c.NotebookApp.ip = '*'" >> $CONFIG
+echo "c.NotebookApp.open_browser = False" >> $CONFIG
+echo "c.NotebookApp.open_browser = False" >> $CONFIG
+echo "c.NotebookApp.open_browser = False" >> $CONFIG
+echo "c.NotebookApp.certfile = '$CERT_PATH'" >> $CONFIG
+echo "c.NotebookApp.keyfile = '$KEY_PATH'" >> $CONFIG
 
 # clone the fast.ai course repo and prompt to start notebook
 cd ~
